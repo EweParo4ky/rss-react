7d97092739f30bfeee09a8af96fd3adf4a31cc6d
Ilya Sukharev LapTop <imsworkfor@gmail.com>
2023-09-13 15:33:41 +0300

    add stuff)

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -5,7 +5,7 @@ export default {
       lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
       inputPlaceholder: 'Ссылка RSS',
       submitBtn: 'Добавить',
-      example: 'https://www.sport.ru/rss/',
+      example: 'http://www.sports.ru/rss/main.xml',
       validationErrors: {
         notOneOf: 'RSS уже существует',
         mustBeUrl: 'Ссылка должна быть валидным URL',
